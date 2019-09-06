985cb3eed224fccf64a9ffde24c41990fa131ebe
Geo Gecko - Uganda <info@geogecko.com>
2019-09-06 11:37:19 +0300

    Update

--- js/map.js
+++ js/map.js
@@ -46,9 +46,9 @@ $.getJSON("data/Kiboga.geojson", function (data) {
 
 d3.selectAll(".slider").append("div").attr("class", "sliders");
 var sliders = document.getElementsByClassName('sliders');
-var fieldName = ['NDVI_JFM', 'NDVWI_JFM', 'Soil_Phosp', 'Soil_Alumi', 'Soil_Potas', 'Soil_Boron', 'Rainfall', 'ppp_sum', 'Land_Cover', 'Slope', 'Elevation'];
+var fieldName = ['NDVI_JFM', 'NDWI_JFM', 'Soil_Phosp', 'Soil_Alumi', 'Soil_Potas', 'Soil_Boron', 'Rainfall', 'ppp_sum', 'Land_Cover', 'Slope', 'Elevation'];
 
-var minMax = [[-0.02,1], [-0.05,1], [600,2300], [700,970], [130,280], [45,150], [0,70], [0,3000], [1,8], [1,20], [1055,1570]];
+var minMax = [[-0.02, 1], [-0.05, 1], [600, 2300], [700, 970], [130, 280], [45, 150], [0, 70], [0, 3000], [1, 8], [1, 20], [1055, 1570]];
 
 // console.log(sliders)
 // console.log(map)
@@ -100,39 +100,50 @@ function addValues() {
   }
 
   var sliderData = [fieldName].concat([realRange]);
-  
+
+
   var filtered = [];
+  var filteredIDs = [];
   var subCountyValue;
 
   for (key in layer['_layers']) {
     var l = layer['_layers'][key];
-    l.setStyle({
-      opacity: 0,
-      fillOpacity: 0
-    })
     for (var j = 0; j < sliderData[0].length; j++) {
       if (l['feature']['properties'][sliderData[0][j]]) {
-        for (var k = 0; k < sliders.length; k++) {
-            subCountyValue = +(l['feature']['properties'][sliderData[0][j]]);
+        subCountyValue = +(l['feature']['properties'][sliderData[0][j]]);
+        // console.log(+((sliderData[1][j][0])));
+        if (subCountyValue < +((sliderData[1][j][0])) || subCountyValue > +((sliderData[1][j][1]))) {
+          filtered.push(l['feature']['properties'].id);
         }
-        if (subCountyValue <= +((sliderData[1][j][0])) || subCountyValue >= +((sliderData[1][j][1]))) {
-          console.log(1);
-          l.setStyle({
-            opacity: 1,
-            fillOpacity: 0.7
-          })
       }
-    }
-    // if (l['feature']['properties']['NDVI_JFM'] > parseFloat(realRange[0][0]) && l['feature']['properties']['NDVI_JFM'] < parseFloat(realRange[0][1])) {
-      
-    // } else {
-     
 
-    // }
+    }
 
   }
 
-}
+  filteredIDs = filtered.filter(function (item, pos) {
+    return filtered.indexOf(item) === pos;
+  });
+
+  console.log(filteredIDs)
+
+  for (key in layer['_layers']) {
+    var l = layer['_layers'][key];
+    l.setStyle({
+      opacity: 1,
+      fillOpacity: 1
+    })
+    for (var j = 0; j < filteredIDs.length; j++) {
+      if (l['feature']['properties'].id === filteredIDs[j]) {
+        l.setStyle({
+          opacity: 0,
+          fillOpacity: 0
+        })
+      }
+
+    }
+
+  }
 }
 
 
